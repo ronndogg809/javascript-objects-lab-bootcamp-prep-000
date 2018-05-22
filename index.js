b318ddffa21d1a4d0de0717ce59100e28df62163
Ronald nunez <ronaldcuello11@gmail.com>
2018-05-22 21:49:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+var recipes = {};
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]: value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object
+}
+function deleteFromObjectByKEy(object, key){
+  delete object.key;
+  return object
+}
